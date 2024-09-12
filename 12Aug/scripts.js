20a5fbdba514c37e8800776a50fb5a9c6acf0588
ctoic <i202612@nu.edu.pk>
2024-09-12 11:24:37 +0500

    added form handling

--- 12Aug/scripts.js
+++ 12Aug/scripts.js
@@ -1,39 +1,36 @@
-function generateTable(form)
-{
-    var amount;
-    var rate;
-    var years;
-    var interest;
-    var table;
-    var year=1;
+function generateTable() {
+    var amount, rate, years, interest, table, year = 1;
 
     amount = document.getElementById('deposit').valueAsNumber;
     rate = document.getElementById('rate').valueAsNumber;
     years = document.getElementById('years').valueAsNumber;
 
+    table = "<table border='1'><tr> <th>Year</th><th>Starting Value</th><th>Interest Earned</th><th>Ending Value</th></tr>";
 
-    table = 
-    "<table>" + "<tr> <th>Year</th><th>Starting value</th></tr>" + "<th>Interest Earned</th><th>Ending Value</th>"
- 
-    while(year <= years)
-    {
-        table+="<tr>";
-        table+="<td>" + year + "</td>";
-        table+="<td>" + amount.tofixed(2) + "</td>";
-        interest = amount * (rate/100);
-        table+="td"+ interest.tofixed(2) + "</td>";
+    while (year <= years) {
+        table += "<tr>";
+        table += "<td>" + year + "</td>";
+        table += "<td>" + amount.toFixed(2) + "</td>";
+        interest = amount * (rate / 100);
+        table += "<td>" + interest.toFixed(2) + "</td>";
         amount = amount + interest;
-        table+="<td>" + amount.tofixed(2) + "</td>";
+        table += "<td>" + amount.toFixed(2) + "</td>";
+        table += "</tr>";
 
-
-        table+="</tr>";
-
-
-        years++;
-        // end of the  while loop
-        document.getElementById('result').innerHTML = table;
+        year++;
     }
-     
-    table+="</table>";
 
+    table += "</table>";
+
+    document.getElementById('result').innerHTML = table;
 }
+
+
+function changeColor() {
+    // Get the selected color value from the dropdown
+    var selectedColor = document.getElementById('color-select').value;
+
+    // Change the color of the "Hello World" h3 tag
+    document.getElementById('hello-world').style.color = selectedColor;
+}
+
